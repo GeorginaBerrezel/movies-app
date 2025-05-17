f4f7e6b1918c391f8ffb8ec38fd468f37b4ffd7b
Georgi 🧡 <georgina.berrezel@gmail.com>
2025-05-17 16:44:40 +0200

    styling cards to correct order

--- src/app/components/MovieCard.jsx
+++ src/app/components/MovieCard.jsx
@@ -2,16 +2,19 @@ import styles from "./Test.module.css";
 
 export default function MovieCard({ movie }) {
   return (
-    <li className={styles.card}>
-        <div className={styles.header}>
+    <li className={styles.card} style={{ backgroundImage: `url(${movie.image})` }}>
+        <div className={styles.overlay}>
+            <img src={movie.image} alt={movie.title} className={styles.image} />
+            <div className={styles.header}>
+                {/* <h3>{movie.title}</h3> */}
+            </div>
+            <div className={styles.body}>
             <h3>{movie.title}</h3>
-        </div>
-        <div className={styles.body}>
-            <img src={movie.image} alt={movie.title} />
             <p>Durée : {movie.duration}</p>
             <div className={styles.extraInfo}>
-            <p>Type : {movie.type}</p>
-            <p>Note : {movie.rate}/5</p>
+                <p>Type : {movie.type}</p>
+                <p>Note : {movie.rate}/5</p>
+            </div>
             </div>
         </div>
     </li>
